a3611864176badf8965224dd934ae645fef3d52b
JackMurrieta <jack.murrieta252851@potros.itson.edu.mx>
2026-09-17 00:24:19 -0700

    terminado

--- app/src/main/java/com/example/guessthenumber_murrietajack/MainActivity.kt
+++ app/src/main/java/com/example/guessthenumber_murrietajack/MainActivity.kt
@@ -1,20 +1,87 @@
 package com.example.guessthenumber_murrietajack
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import kotlinx.coroutines.withTimeoutOrNull
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+    var minValue = 0
+    var maxValue = 100
+    var num : Int = 0
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val guessing: TextView = findViewById(R.id.guessing)
+        val down: Button = findViewById(R.id.down)
+        val up: Button = findViewById(R.id.up)
+        val generate: Button = findViewById(R.id.generate)
+        val guessed: Button = findViewById(R.id.guessed)
+
+        generate.setOnClickListener {
+            num = Random.nextInt(minValue,maxValue)
+            guessing.setText(num.toString())
+            generate.visibility = View.INVISIBLE
+            guessed.visibility= View.VISIBLE
+        }
+
+        up.setOnClickListener {
+            minValue = num
+            if(checkingLimits()){
+                num = Random.nextInt(minValue,maxValue)
+                guessing.setText(num.toString())
+            }else{
+                guessing.setText(" No puede ser me ganaste :(")
+            }
+        }
+
+        down.setOnClickListener {
+            maxValue = num
+            num = Random.nextInt(minValue,maxValue)
+            guessing.setText(num.toString())
+        }
+
+        guessed.setOnClickListener {
+            var won = false
+            if(!won) {
+                guessing.setText("Adivine tu numero es el: " + num)
+                guessed.setText("Volver a jugar")
+                won = true
+
+            }else{
+                generate.visibility = View.VISIBLE
+                guessing.setText("Tap on generate to start")
+                guessed.visibility= View.GONE
+                resetValue()
+            }
+        }
+
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    
+    fun resetValue(){
+
+    }
+
+    fun checkingLimits(): Boolean{
+        return minValue != maxValue
     }
 }
